6d0a84ea690286fff1d66b9c4d2d86a58b613936
Felipe Côrtes <contatos.cortes@gmail.com>
2020-05-12 13:15:50 -0300

    Pacote

---
+++ pacote/reuso/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kyfelipe/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(5.0, 2.0))
+	//fmt.Println(area._TrianguloEq(5.0, 2.0))
+}
